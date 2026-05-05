e7dc5a0fe4cd030103195c67c675536c4a9a88cf
Ollie Joestar <bigzzzid@gmail.com>
2026-05-05 10:36:02 +0200

    rounding up of API info

--- src/DataTypes.ts
+++ src/DataTypes.ts
@@ -14,18 +14,3 @@ export interface GhostData {
   frames: GhostFrame[]
 }
 
-export interface Checkpoints {
-  0: [number, number, number]        // position x, y, z
-  1: [number, number, number]
-  2: [number, number, number]
-}
-
-
-export interface AIFrame {
-  position: [number, number, number]        // position x, y, z
-  quaternion: [number, number, number, number] // quaternion x, y, z, w
-  velocity: [number, number, number]        // velocity x, y, z
-  totalChekpoints: number                    // total number of checkpoints in track
-  currentCheckpoint: number                    // index of next checkpoint to reach
-  checkpoints: Checkpoints[]                  // array of next 3 checkpoint positions
-}
